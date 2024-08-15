4a5019b76ecda23fc57965ced300f6502e3313e7
Koalaphant <andrew.wardjones@icloud.com>
2024-08-15 15:54:08 +0100

    restyled navbar and logo

--- components/NavSection/NavBar/NavBar.tsx
+++ components/NavSection/NavBar/NavBar.tsx
@@ -4,17 +4,18 @@ import NavLink from "../NavLinks/NavLinks"; // Make sure to adjust the path as n
 export default function NavBar() {
   const links = [
     { label: "Home", href: "/" },
-    { label: "About", href: "/about" },
+    { label: "Posts", href: "/posts" },
     { label: "Contact", href: "/contact" },
   ];
 
   return (
-    <nav className="bg-orange-300 flex flex-col items-center justify-center py-4 sm:flex-row sm:items-center sm:justify-between sm:px-5">
-      <img
-        src="https://placehold.co/100x50"
-        alt="Logo"
-        className="mb-4 sm:mb-0 sm:mr-4"
-      />
+    <nav className="bg-red-800 flex flex-col items-center justify-center py-4 sm:flex-row sm:items-center sm:justify-between sm:px-5 lg:px-10">
+      <div className="flex justify-center items-center gap-3 lg:gap-2">
+        <img src="/lfc-logo.svg" alt="Logo" className="mb-4 sm:mb-0 w-[50px]" />
+        <h1 className="text-4xl text-white">
+          <span className="font-extrabold">LFC</span>Blog
+        </h1>
+      </div>
       <div className="flex space-x-4">
         {links.map((link) => (
           <NavLink key={link.label} label={link.label} href={link.href} />
--- components/NavSection/NavLinks/NavLinks.tsx
+++ components/NavSection/NavLinks/NavLinks.tsx
@@ -17,7 +17,9 @@ export default function NavLink({ label, href }: NavLinkProps) {
     <Link
       href={href}
       className={`px-3 py-2 ${
-        isActive ? "text-white bg-blue-500" : "text-blue-700"
+        isActive
+          ? "text-white font-extrabold underline underline-offset-4"
+          : "text-white"
       }`}
     >
       {label}
