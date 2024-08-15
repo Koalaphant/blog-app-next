90eae41829e88d9207260e7f82a5c04ba35a9f1b
Koalaphant <andrew.wardjones@icloud.com>
2024-08-15 13:22:39 +0100

    adjusted styling

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,14 @@
 export default function Home() {
   return (
-    <>
-      <h1>Hey</h1>
-    </>
+    <main className="bg-sky-500 h-full items-center justify-center flex flex-col">
+      <div className="space-y-6 text-center">
+        <h1 className="text-6xl text-white font-semibold drop-shadow-md">
+          🔐 Auth
+        </h1>
+        <p className="text-lg text-white drop-shadow-sm">
+          A simple authentication service
+        </p>
+      </div>
+    </main>
   );
 }
