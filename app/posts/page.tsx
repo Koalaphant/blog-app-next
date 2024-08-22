619a2a5903b8cd2509d2ca736541aa998233b768
Koalaphant <andrew.wardjones@icloud.com>
2024-08-22 23:36:41 +0100

    fetched posts for the post page - showing all posts

--- app/posts/page.tsx
+++ app/posts/page.tsx
@@ -1,8 +1,37 @@
-import React from 'react'
-import { Prisma } from '@prisma/client'
+import RecentPosts from "@/components/Blog Section/RecentPosts";
+import React from "react";
 
-export default function page() {
+interface Post {
+  id: number;
+  title: string;
+  content: string;
+  createdAt: string;
+  like_rating: number;
+  featured_image_url: string;
+}
+
+async function fetchPosts() {
+  const response = await fetch("http://localhost:3000/api/posts", {
+    next: { revalidate: 5 },
+  });
+
+  if (!response.ok) {
+    throw new Error("Network response was not ok");
+  }
+
+  const data = await response.json();
+  return data;
+}
+
+export default async function page() {
+  const data: Post[] = await fetchPosts();
   return (
-    <div>page</div>
-  )
+    <div className="max-w-6xl mx-auto my-20">
+      <div className="grid sm:grid-cols-2 md:grid-cols-2 xl:grid-cols-3 gap-5 sm:gap-5 md:gap-y-10 md:gap-x-10 mb-10">
+        {data.map((post) => (
+          <RecentPosts key={post.id} post={post} />
+        ))}
+      </div>
+    </div>
+  );
 }
