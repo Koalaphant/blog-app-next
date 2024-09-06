5969f1678cc427c76137781a8962a37063e707b0
Koalaphant <andrew.wardjones@icloud.com>
2024-09-07 00:41:15 +0100

    removed auth feature due to bugs

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -2,12 +2,10 @@
 
 import { useState } from "react";
 import { useFormState, useFormStatus } from "react-dom";
-import { authenticate } from "@/lib/actions";
 
 export default function Page() {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
-  const [errorMessage, dispatch] = useFormState(authenticate, undefined);
 
   function handleEmail(e: React.ChangeEvent<HTMLInputElement>) {
     setEmail(e.target.value);
@@ -20,7 +18,7 @@ export default function Page() {
   return (
     <div className="flex justify-center items-center min-h-screen bg-gray-100">
       <div className="bg-red-800 p-8 rounded-lg shadow-lg w-full max-w-sm">
-        <form action={dispatch} className="flex flex-col space-y-6">
+        <form className="flex flex-col space-y-6">
           <div>
             <label className="text-white block text-sm font-semibold mb-2">
               Email:
@@ -46,34 +44,8 @@ export default function Page() {
               value={password}
             />
           </div>
-
-          <LoginButton />
-          <div
-            className="flex h-8 items-end space-x-1"
-            aria-live="polite"
-            aria-atomic="true"
-          >
-            {errorMessage && (
-              <>
-                <p>{errorMessage}</p>
-              </>
-            )}
-          </div>
         </form>
       </div>
     </div>
-  );
-}
-
-function LoginButton() {
-  const { pending } = useFormStatus(); // Ensure this is used or remove it if not needed
-  return (
-    <button
-      aria-disabled={pending}
-      type="submit"
-      className="w-full py-2 bg-white text-red-800 font-semibold rounded-md hover:bg-gray-200 focus:outline-none focus:ring-2 focus:ring-red-500"
-    >
-      Sign In
-    </button>
   );
 }
