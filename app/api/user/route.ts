322adc0cb0831fdf09773676f0d8d9b2583a8c55
Koalaphant <andrew.wardjones@icloud.com>
2024-09-10 13:38:17 +0100

    create register user api

---
+++ app/api/user/route.ts
@@ -0,0 +1,44 @@
+import { db } from "@/lib/db";
+import { NextResponse } from "next/server";
+
+export async function POST(req: Request) {
+  try {
+    const body = await req.json();
+
+    const { email, username, password } = body;
+
+    //check if email exists already
+    const existingEmailByUsername = await db.user.findUnique({
+      where: { email: email },
+    });
+
+    if (existingEmailByUsername) {
+      return NextResponse.json(
+        { user: null, message: "user with this email already exists" },
+        { status: 409 }
+      );
+    }
+
+    //check if username exists already
+    const existingUserByUsername = await db.user.findUnique({
+      where: { username: username },
+    });
+
+    if (existingUserByUsername) {
+      return NextResponse.json(
+        { user: null, message: "user with this username already exists" },
+        { status: 409 }
+      );
+    }
+
+    const newUser = await db.user.create({
+      data: {
+        email,
+        username,
+        password,
+      },
+    });
+
+    return NextResponse.json(body);
+  } catch (error) {}
+}
