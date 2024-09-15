df8445035e7cedf87b6927079421a13ebe38aa5e
Koalaphant <andrew.wardjones@icloud.com>
2024-09-15 22:34:43 +0200

    dashboard styling

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -2,16 +2,26 @@ import { authOptions } from "@/lib/auth";
 import { getServerSession } from "next-auth";
 
 const page = async () => {
-  const session = await getServerSession(authOptions)
+  const session = await getServerSession(authOptions);
 
-  if(session?.user){
-    return <h1>Welcome back {session?.user.username}</h1>
+  if (session?.user) {
+    return (
+      <div className="flex justify-center">
+        <div className="m-5 bg-red-800 px-5 py-4">
+          <h1 className="text-white">
+            Welcome back{" "}
+            {session.user.username[0].toUpperCase() +
+              session.user.username.slice(1)}
+          </h1>
+        </div>
+      </div>
+    );
   }
   return (
     <div>
       <h2>Please login to see the dashboard</h2>
     </div>
-  )
-}
+  );
+};
 
 export default page;
