91d03cc4d15ddc8b0a0b5850ccd7e0e086bb1372
Koalaphant <andrew.wardjones@icloud.com>
2024-09-22 10:37:53 +0200

    adding title and caption to contact page

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -1,7 +1,10 @@
-import React from 'react'
+import React from "react";
 
 export default function page() {
   return (
-    <div>page</div>
-  )
+    <div className="m-5">
+      <h1 className="text-red-800 font-bold text-3xl mb-1">Contact Us</h1>
+      <p className="">Let us know what you think of our content here...</p>
+    </div>
+  );
 }
